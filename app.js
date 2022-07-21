49043224f341c9a6e2bcb166f735c9d7b8a461cf
Nini-ane <108152961+Nini-ane@users.noreply.github.com>
2022-07-21 22:45:31 +0200

    played with css styling, added forecast cards

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ let forecastElement = document.querySelector("#forecast");
 let forecastHtml = `<div class="row">`;
 let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
 days.forEach(function (day) {
-  forecastHtml = forecastHtml + `<div class="firstDay col-2">
+  forecastHtml = forecastHtml + `<div class="forecastDays col-2">
              <span id="day-one">
                 <img src="media/partly-cloudy.png" id="icon-day-one">
                 <br />
